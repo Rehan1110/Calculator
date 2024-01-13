29f965f0a3dc47f198ff124b3b9dfcb0b448b913
Rehan Abbas <abbasrehan054@gmail.com>
2024-01-13 16:13:07 +0530

    added the clear Display function

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let DisplayBox=document.querySelector('.Display-box');
+let currentValue='';
 function appendValue(v){
+  currentValue +=v;
  DisplayBox.value=v;
 }
 function ClearDisplay(){
+  currentValue='';
   DisplayBox.value=' ';
 }
+// function Calculate(){
+//   try{
+//     currentValue = eval(currentValue).toString();
+//     DisplayBox.value=currentValue;
+//   } catch(error){
+//     DisplayBox.value='Error';
+//   }
+// }
